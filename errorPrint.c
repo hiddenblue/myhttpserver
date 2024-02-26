4d8252c869a49ce695d699a368e851d9b4ba7c10
hiddenblue <freewayrong@foxmail.com>
2024-02-26 13:48:26 +0800

    move some code from server.c to HandleTCPclient()

--- errorPrint.c
+++ errorPrint.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "errorPrint.h"
 
 void DieWithUserMessage(const char *msg)
@@ -14,4 +13,35 @@ void DieWithSystemMessage(const char *msg)
     fputs(msg, stderr);
     fputs("\n", stderr);
     exit(-1);
+}
+
+void HandleTCPClient(int clientSocket)
+{
+    // server recv
+    char buf[BUFSIZ];
+    ssize_t numByteRecv = recv(clientSocket, buf, BUFSIZ - 1, 0);
+    if (numByteRecv < 0)
+        DieWithSystemMessage("recv() failed");
+    buf[numByteRecv] = '\0';
+
+    fputs("Server received message: ", stdout);
+    fputs(buf, stdout);
+
+    if (strcmp(buf, "GET\n") == 0)
+        DieWithUserMessage("did'nt recv GET request");
+
+    char *sendString = "HTTP/1.1 400 Bad Request\n";
+    fputs("server Send message: ", stdout);
+    fputs("\n", stdout);
+    fputs(sendString, stdout);
+
+    // server send
+    size_t sendStringLen = strlen(sendString) + 1;
+
+    ssize_t numBytesent = send(clientSocket, sendString, sendStringLen, 0);
+    if (numBytesent == -1)
+        DieWithSystemMessage("send() failed");
+
+    fprintf(stdout, "\n");
+    close(clientSocket);
 }
--- errorPrint.h
+++ errorPrint.h
@@ -1,4 +1,11 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
 
 #ifndef __ERRORPRINT_H
 #define __ERRORPRINT_H
@@ -6,4 +13,6 @@
 void DieWithUserMessage(const char *msg);
 
 void DieWithSystemMessage(const char *msg);
+
+void HandleTCPClient(int clientSock);
 #endif
