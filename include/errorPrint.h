e8cf7be01d9b08d78529695c0cb3877b4a307f09
hiddenblue <freewayrong@foxmail.com>
2024-03-01 21:27:49 +0800

    add improved error printing and locating macro

--- include/errorPrint.h
+++ include/errorPrint.h
@@ -11,7 +11,20 @@
 #ifndef __ERRORPRINT_H
 #define __ERRORPRINT_H
 
-#define PRINTERROR(str) fprintf(stdout, "\033[31mFunction  %s() error at file %s line %d Info: %s\033[0m\n", __func__, __FILE__, __LINE__, str);
+// 这一行绝对不能放在某个报错函数里面，因为这里面的宏只能检测当前的环境
+
+// 为了方便在str添加%d %s类型的参数，可以借助sprintf和snrpintf等函数
+#define PRINTERROR(str_format) \
+    char errorBuf[BUFSIZ];     \
+    sprintf(errorBuf, str);    \
+    fprintf(stderr, "\033[31mFunction  %s() error at file %s line %d Info: %s\033[0m\n", __func__, __FILE__, __LINE__, errorBuf);
+
+#define PRINTERROREXIT(str_format)                                                                                           \
+    char errorBuf[BUFSIZ];                                                                                                   \
+    sprintf(errorBuf, str);                                                                                                  \
+    fprintf(stderr, "\033[31mFunction  %s() error at file %s line %d Info: %s\033[0m\n", __func__, __FILE__, __LINE__, str); \
+    fprintf(stderr, "exiting");                                                                                              \
+    EXIT(EXIT_FAILURE);
 
 void DieWithUserMessage(const char *msg);
 
